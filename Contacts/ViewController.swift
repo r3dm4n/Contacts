9ee63e76c9059694346d2c940e4cb82df6662eba
r3dm4n <r3dm4n@me.com>
2017-12-16 21:26:30 +0000

    UITableView with twoDimensionalArray

--- Contacts/ViewController.swift
+++ Contacts/ViewController.swift
@@ -8,12 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    let cellId = "cellId"
+    
+    let twoDimensionalArray = [
+        ["Amy", "Bill", "Zack", "Steve", "Jack", "Jill", "Mary"],
+        ["Carl", "Chris", "Christina", "Cameron"],
+        ["David", "Dan"],
+        ["Patrick", "Patty"]
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.title = "Contacts"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.backgroundColor = .lightGray
+        label.text = "Header"
+        return label
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return twoDimensionalArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return twoDimensionalArray[section].count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
         
+        
+        let name = twoDimensionalArray[indexPath.section][indexPath.row]
+        
+        cell.textLabel?.text = name
+        
+        cell.textLabel?.text = "\(name) Section:\(indexPath.section) Row\(indexPath.row)"
+        
+        return cell
     }
 
 }
